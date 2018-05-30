f4182ac541b4b6ebac219a91966d11774b94369e
narendar4100 <39778990+narendar4100@users.noreply.github.com>
2018-05-30 16:43:47 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Narendar reddy
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+        System.out.println("hello java");
+     int n1=10;
+     float f1=1.5f;
+     double d1=12.434343434;
+     short s1=12;
+     char c1='a';
+     String str1="java";
+             boolean b1= true;
+             System.out.println("value of n is " + n1);
+             System.out.println("value of n is " + f1);
+             System.out.println("value of f is " + d1);
+             System.out.println("value of d is " + s1);
+             System.out.println("value of c is " + c1);
+             System.out.println("value of string is " + str1);
+             n1*=20;
+             System.out.println("value of n is " + n1);
+             n1++;
+             System.out.println("value of n is " + n1);
+             n1--;            
+             System.out.println("value of n is " + n1);
+             ++n1;
+             System.out.println("value of n is " + n1);
+             c1='A';
+                          System.out.println("value of n is " + c1);
+                                       System.out.println("value of n is " + (c1+10));
+                                       c1++;
+                                       System.out.println("value of n is " + (c1+10));
+                                       c1--;
+                                       System.out.println("value of n is " + (c1));
+                                       c1--;
+                                       System.out.println("value of n is " + (c1));
+                                       
+                                       
+             
+           
+                                                                                           
+    }
+}
